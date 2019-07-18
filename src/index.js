a10c74698aa892040e8536e616ca09a3471c7ce8
Dmitry <dmitryshelomanov@mail.ru>
2019-07-18 16:49:46 +0300

    refactor: remove endless cycle & update snake type

--- src/index.js
+++ src/index.js
@@ -22,35 +22,20 @@ import {
   headSnake,
   drawSnake,
   tailSnake,
+  addPeaceOfSnake,
+  setDirection,
 } from './snake'
 import { breadthFirstSearch } from './algorithms'
 import 'reset-css'
-
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
 
 function main(canvas, ctx) {
   const localSize = getLocalSize(pageWidth, pageHeight)
   const globalSize = getGlobalSize(localSize.w, localSize.h)
 
-  /*
-
-  snake: [
-    {
-      id: number,
-      body: [x: number, y: number]
-    }
-  ]
-
-  */
-
   const state = {
     isPressed: false,
-    direction: DIRECTIONS.RIGHT,
     snake: buildSnake(randomPosition()),
     apple: randomPosition(),
-    path: [],
-    processed: [],
   }
 
   const nextTick = createTimeController(interval)
@@ -58,56 +43,67 @@ function main(canvas, ctx) {
   convigureCanvas(canvas, localSize, globalSize)
 
   nextTick.start(() => {
-    clearCell(ctx, [...state.snake, ...state.processed])
+    clearSnake(ctx, state.snake)
 
     const result = breadthFirstSearch(
-      localSize,
       getIndexByPosition(headSnake(state.snake)),
       getIndexByPosition(state.apple),
       (index) => {
         const [x, y] = getPositionByIndex(index)
 
-        return state.snake.find(([x1, y1]) => x1 === x && y1 === y)
+        return state.snake.body.find(([x1, y1]) => x1 === x && y1 === y)
       }
     )
 
-    state.path = result.path
-    state.processed = result.processed
+    const nextPosition =
+      result.path[0] ||
+      getNextPositionByDirection(headSnake(state.snake), state.snake.dir)
 
-    const nextPosition = state.path.shift()
+    setDirection(
+      state.snake,
+      getDirectionByPosition(headSnake(state.snake), nextPosition)
+    )
 
-    const dir = getDirectionByPosition(headSnake(state.snake), nextPosition)
+    moveSnake(state.snake, nextPosition)
 
-    state.snake = moveSnake(state.snake, nextPosition)
     state.isPressed = false
 
     oneToOneCollision(nextPosition, state.apple, () => {
       state.apple = randomPosition()
 
-      const peaceOfSnake = getNextPositionByDirection(nextPosition, dir)
+      const peaceOfSnake = getNextPositionByDirection(
+        nextPosition,
+        state.snake.dir
+      )
 
-      state.snake.push(peaceOfSnake)
+      addPeaceOfSnake(state.snake, peaceOfSnake)
     })
 
     drawSnake(ctx, state.snake)
 
-    oneToManyCollision(headSnake(state.snake), tailSnake(state.snake), () => {
-      console.log(
-        'crash',
-        { nextPosition, head: headSnake(state.snake) },
-        state
-      )
-      clearCell(ctx, [...state.snake, ...state.processed])
+    oneToManyCollision(
+      headSnake(state.snake),
+      tailSnake(state.snake),
+      (crashed) => {
+        console.log('crash', { head: headSnake(state.snake), crashed })
 
-      drawSnake(ctx, state.snake)
+        clearSnake(ctx, state.snake)
 
-      nextTick.pause()
-    })
+        state.snake.isCrash = true
+
+        drawSnake(ctx, state.snake)
+
+        nextTick.pause()
+      }
+    )
 
     drawSquare(ctx, state.apple, 'rgb(238, 68, 0)')
   })
 
   drawGrid(ctx)
 }
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
 
 main(canvas, ctx)
--- src/snake.js
+++ src/snake.js
@@ -1,44 +1,56 @@
-import { convertLocalPositionToGlobal } from './utils'
+import { convertLocalPositionToGlobal, randomId } from './utils'
 import { borderSize, cellSize } from './config'
 import { drawSquare } from './renderer'
-import { checkCollision } from './collision'
+import { DIRECTIONS } from './controll'
 
 export function buildSnake(headPosition) {
-  return [headPosition, [headPosition[0] + 1, headPosition[1]]]
+  return {
+    id: randomId(),
+    body: [headPosition, [headPosition[0] + 1, headPosition[1]]],
+    isCrash: false,
+    dir: DIRECTIONS.RIGHT,
+  }
 }
 
 export function moveSnake(snake, nextPosition) {
-  const [_, ...rest] = snake
+  const [_, ...rest] = snake.body
 
   if (nextPosition) {
-    return [...rest, nextPosition]
+    snake.body = [...rest, nextPosition]
   }
-
-  return snake
 }
 
 export function clearSnake(ctx, snake) {
-  for (let i = 0; i < snake.length; i++) {
+  for (let i = 0; i < snake.body.length; i++) {
     const size = cellSize - borderSize * 4
-    const [x, y] = convertLocalPositionToGlobal(snake[i])
+    const [x, y] = convertLocalPositionToGlobal(snake.body[i])
 
     ctx.clearRect(x + borderSize + 2, y + borderSize + 2, size, size)
   }
 }
 
 export function headSnake(snake) {
-  return snake[snake.length - 1]
+  return snake.body[snake.body.length - 1]
 }
 
 export function tailSnake(snake) {
-  return snake.slice(1, snake.length - 1)
+  return snake.body.slice(1, snake.body.length - 1)
 }
 
-export function drawSnake(ctx, snake = []) {
-  for (let i = 0; i < snake.length; i++) {
-    const isHead = i === snake.length - 1
+export function drawSnake(ctx, snake) {
+  for (let i = 0; i < snake.body.length; i++) {
+    const isHead = i === snake.body.length - 1
     const color = isHead ? 'rgb(0, 221, 0)' : 'rgb(152, 251, 152)'
+    const crashedColor = isHead ? 'rgba(0, 0, 0, 1)' : 'rgba(0, 0, 0, 0.6)'
 
-    drawSquare(ctx, snake[i], color)
+    drawSquare(ctx, snake.body[i], snake.isCrash ? crashedColor : color)
   }
+}
+
+export function addPeaceOfSnake(snake, peaceOfSnake) {
+  snake.body.push(peaceOfSnake)
+}
+
+export function setDirection(snake, dir) {
+  snake.dir = dir
 }
