2dd520b66c11d8400df8d03dcbde0c8dd49ca18f
Dmitry Shelomanov <userbq201@mail.ru>
2019-07-19 14:39:56 +0300

    chore: refactore

--- src/snake.js
+++ src/snake.js
@@ -9,6 +9,8 @@ export function buildSnake(headPosition) {
     body: [headPosition, [headPosition[0] + 1, headPosition[1]]],
     isCrash: false,
     dir: DIRECTIONS.RIGHT,
+    weight: 1,
+    score: 0,
   }
 }
 
@@ -37,12 +39,13 @@ export function tailSnake(snake) {
   return snake.body.slice(1, snake.body.length - 1)
 }
 
-export function drawSnake(ctx, snake) {
+export function drawSnake(ctx, snake, callback) {
   for (let i = 0; i < snake.body.length; i++) {
     const isHead = i === snake.body.length - 1
     const color = isHead ? 'rgb(0, 221, 0)' : 'rgb(152, 251, 152)'
     const crashedColor = isHead ? 'rgba(0, 0, 0, 1)' : 'rgba(0, 0, 0, 0.6)'
 
+    callback(i, snake.body[i])
     drawSquare(ctx, snake.body[i], snake.isCrash ? crashedColor : color)
   }
 }
@@ -53,4 +56,8 @@ export function addPeaceOfSnake(snake, peaceOfSnake) {
 
 export function setDirection(snake, dir) {
   snake.dir = dir
+}
+
+export function eatTarget(snake) {
+  snake.score += 1
 }
