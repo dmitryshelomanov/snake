4ec989dfeec4ff9e79ae2fc0e74590639f8adfde
Dmitry <dmitryshelomanov@mail.ru>
2019-07-23 13:22:39 +0300

    chore: refactore

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const pageWidth = 1000
 export const pageHeight = 1000
 export const fps = 60
 export const borderSize = 1
-export const interval = 100 / 30
+export const interval = 100
 export const boardLength = pageWidth * pageHeight
