f151a5cd68ac6f9873bd4566c6c271523cd93cb5
Dmitry Shelomanov <userbq201@mail.ru>
2019-07-23 13:21:57 +0300

    refactor: render score

--- src/score.js
+++ src/score.js
@@ -1,5 +1,30 @@
-const scoreBoard = document.querySelector('#score')
+const scoreBoardList = document.querySelector('.board-list')
 
-export function setScore(score) {
-  scoreBoard.textContent = score
+const scores = {}
+
+export function setScore(snakeId, { score, color }) {
+  if (!scores[`snake-${snakeId}`]) {
+    const wrapper = document.createElement('div')
+    const titleWrapper = document.createElement('p')
+    const title = document.createTextNode('Score: ')
+    const scoreCounterWrapper = document.createElement('p')
+
+    scoreCounterWrapper.setAttribute('id', `snake-${snakeId}`)
+    wrapper.classList.add('board')
+    wrapper.style.color = color
+
+    titleWrapper.appendChild(title)
+    wrapper.appendChild(titleWrapper)
+    wrapper.appendChild(scoreCounterWrapper)
+
+    scores[`snake-${snakeId}`] = {
+      wrapper,
+      titleWrapper,
+      scoreCounterWrapper,
+    }
+
+    scoreBoardList.appendChild(wrapper)
+  }
+
+  scores[`snake-${snakeId}`].scoreCounterWrapper.innerText = score
 }
