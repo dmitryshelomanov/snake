c1e31aa5fa80eedb01e34d282292f90d70ea81ca
Dmitry Shelomanov <userbq201@mail.ru>
2019-08-13 14:01:19 +0300

    test: add tests & mock

--- src/controll.test.js
+++ src/controll.test.js
@@ -1,10 +1,10 @@
-import { getDirectionByPosition } from './controll'
+import { getDirectionByPosition, getNextPositionByDirection } from './controll'
 import { DIRECTIONS } from './config'
 
 /*
 Map 4x4
 
-[0,0][1,0][2,0][3,0]
+[0,0][1,0][2,0][3,0] is equals to [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 [0,1][1,1][2,1][3,1]
 [0,2][1,2][2,2][3,2]
 [0,3][1,3][2,3][3,3]
@@ -13,5 +13,12 @@ Map 4x4
 describe('controll', () => {
   it('should return valid direction by position', () => {
     expect(getDirectionByPosition([0, 0], [1, 0])).toBe(DIRECTIONS.RIGHT)
+  })
+
+  it('should return valid position', () => {
+    expect(getNextPositionByDirection([0, 0], DIRECTIONS.RIGHT)).toEqual([1, 0])
+    expect(getNextPositionByDirection([3, 0], DIRECTIONS.LEFT)).toEqual([2, 0])
+    expect(getNextPositionByDirection([3, 1], DIRECTIONS.TOP)).toEqual([3, 0])
+    expect(getNextPositionByDirection([3, 1], DIRECTIONS.DOWN)).toEqual([3, 2])
   })
 })
