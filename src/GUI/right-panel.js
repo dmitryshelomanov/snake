bc62fd2b664d3e5ab1300a5de562f4b9cca6690a
Dmitry Shelomanov <userbq201@mail.ru>
2019-08-25 00:29:49 +0300

    feat: add movable setting board

--- src/GUI/right-panel.js
+++ src/GUI/right-panel.js
@@ -9,7 +9,6 @@ export const Wrapper = styled.div`
   color: #fff;
   border-radius: 8px;
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-  cursor: default;
   width: 100%;
   min-height: 100px;
   max-height: 80vh;
