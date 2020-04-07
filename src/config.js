9a18b792c540079196d85eafc2c6587972904c1c
Dmitry <dmitryshelomanov@mail.ru>
2020-04-07 15:05:18 +0300

    refactor

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import { KEYS } from './keyboard'
 
-export const cellSize = 15
+export const cellSize = 10
 export const pageWidth = window.innerWidth
 export const pageHeight = window.innerHeight
-export const fps = 15
+export const fps = 60
 export const borderSize = 1
 export const boardLength = pageWidth * pageHeight
 
@@ -12,4 +12,16 @@ export const DIRECTIONS = {
   RIGHT: KEYS.RIGHT_ARROW,
   TOP: KEYS.TOP_ARROW,
   DOWN: KEYS.DOWN_ARROW,
+}
+
+export const GAME_STATE = {
+  IS_PLAY: 'play',
+  IS_PAUSE: 'pause',
+}
+
+export const PLACE_TYPE = {
+  EMPTY: 'empty',
+  GAME_OBJECT: 'game_object',
+  BRICK: 'brick',
+  FOOD: 'food',
 }
