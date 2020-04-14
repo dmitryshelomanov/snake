e6d2234f0b7138900de4860f8ed31b67da4d041e
Dmitry <dmitryshelomanov@mail.ru>
2020-04-15 02:51:38 +0300

    refactor(updater-user): add updater for user snake

--- src/updaters/user.ts
+++ src/updaters/user.ts
@@ -2,47 +2,43 @@ import { DIRECTIONS } from '../config'
 import { getNextPositionByDirection } from '../controll'
 import { checkBounds } from '../collision'
 import { keyboradFactory, KEYS } from '../keyboard'
-import { headSnake } from '../models/snake'
+import { headSnake, Snake } from '../models/snake'
 
 const gameInput = keyboradFactory()
 
-export function user(self) {
-  // const headPosition = getNextPositionByDirection(
-  //   headSnake(self),
-  //   self.direction
-  // )
-  // const nextPosition = checkBounds(headPosition)
-  // moveSnake({ id: self.id, nextPosition })
-  // if (
-  //   gameInput.isDown(KEYS.RIGHT_ARROW) &&
-  //   self.direction !== DIRECTIONS.LEFT
-  // ) {
-  //   setDirectionForSnake({
-  //     id: self.id,
-  //     direction: DIRECTIONS.RIGHT,
-  //   })
-  // }
-  // if (gameInput.isDown(KEYS.DOWN_ARROW) && self.direction !== DIRECTIONS.TOP) {
-  //   setDirectionForSnake({
-  //     id: self.id,
-  //     direction: DIRECTIONS.DOWN,
-  //   })
-  // }
-  // if (
-  //   gameInput.isDown(KEYS.LEFT_ARROW) &&
-  //   self.direction !== DIRECTIONS.RIGHT
-  // ) {
-  //   setDirectionForSnake({
-  //     id: self.id,
-  //     direction: DIRECTIONS.LEFT,
-  //   })
-  // }
-  // if (gameInput.isDown(KEYS.TOP_ARROW) && self.direction !== DIRECTIONS.DOWN) {
-  //   setDirectionForSnake({
-  //     id: self.id,
-  //     direction: DIRECTIONS.TOP,
-  //   })
-  // }
+export function user({
+  snake,
+}: {
+  snake: Snake
+}): { nextPosition: Coords; nextDirection: DIRECTIONS } {
+  let { direction } = snake
 
-  return {}
+  if (
+    gameInput.isDown(KEYS.RIGHT_ARROW) &&
+    snake.direction !== DIRECTIONS.LEFT
+  ) {
+    direction = DIRECTIONS.RIGHT
+  }
+
+  if (gameInput.isDown(KEYS.DOWN_ARROW) && snake.direction !== DIRECTIONS.TOP) {
+    direction = DIRECTIONS.DOWN
+  }
+
+  if (
+    gameInput.isDown(KEYS.LEFT_ARROW) &&
+    snake.direction !== DIRECTIONS.RIGHT
+  ) {
+    direction = DIRECTIONS.LEFT
+  }
+
+  if (gameInput.isDown(KEYS.TOP_ARROW) && snake.direction !== DIRECTIONS.DOWN) {
+    direction = DIRECTIONS.TOP
+  }
+
+  return {
+    nextPosition: checkBounds(
+      getNextPositionByDirection(headSnake(snake), direction)
+    ),
+    nextDirection: direction,
+  }
 }
