c75aeb52ed31ff7ff6ecca5859ad8f5a615e1e6f
Dmitry <dmitryshelomanov@mail.ru>
2021-01-11 04:13:18 +0300

    feat(editor): add themes

--- src/GUI/editor.tsx
+++ src/GUI/editor.tsx
@@ -4,7 +4,20 @@ import styled from 'styled-components'
 import AceEditor from 'react-ace'
 import 'ace-builds/src-noconflict/mode-javascript'
 import 'ace-builds/src-noconflict/theme-tomorrow'
-import { $editorCode, changeEditorCode } from '../models/custom-alghorithm'
+import 'ace-builds/src-noconflict/theme-twilight'
+import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/theme-github'
+import 'ace-builds/src-noconflict/theme-kuroir'
+import 'ace-builds/src-noconflict/theme-terminal'
+import 'ace-builds/src-noconflict/theme-solarized_dark'
+import 'ace-builds/src-noconflict/theme-textmate'
+import 'ace-builds/src-noconflict/theme-xcode'
+import {
+  $editorCode,
+  changeEditorCode,
+  $editorTheme,
+  changeTheme,
+} from '../models/custom-alghorithm'
 
 const Wrapper = styled.div`
   position: fixed;
@@ -18,20 +31,53 @@ const Wrapper = styled.div`
   }
 `
 
+const Select = styled.select`
+  position: fixed;
+  top: 25px;
+  right: 25px;
+  z-index: 9999;
+`
+
+const themes = [
+  'monokai',
+  'github',
+  'tomorrow',
+  'kuroir',
+  'twilight',
+  'xcode',
+  'textmate',
+  'solarized_dark',
+  'terminal',
+]
+
 export function Editor() {
   const code = useStore($editorCode)
+  const activeTheme = useStore($editorTheme)
 
   return (
     <Wrapper>
+      <Select
+        name="Theme"
+        value={activeTheme}
+        onChange={(it) => {
+          changeTheme(it.target.value)
+        }}
+      >
+        {themes.map((it) => (
+          <option key={it} value={it} selected={it === activeTheme}>
+            {it}
+          </option>
+        ))}
+      </Select>
       <AceEditor
         value={code}
         onChange={(nextCode) => {
           changeEditorCode(nextCode)
         }}
         mode="javascript"
-        theme="tomorrow"
+        theme={activeTheme}
         name="UNIQUE_ID_OF_DIV"
-        editorProps={{ $blockScrolling: true }}
+        editorProps={{ $blockScrolling: false }}
         fontSize={14}
         showPrintMargin
         showGutter
